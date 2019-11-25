800ae772fbe7591f37e915106df047b5e03e650a
Daniel Gamage <hello@danielgamage.com>
2019-11-24 22:08:32 -0500

    Updates

---
+++ precache-manifest.0aa9519baeff29a3b466a61112329180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22093fbe980bde900e794b1a2385f914",
+    "url": "/hue-pals/index.html"
+  },
+  {
+    "revision": "c553331cd1b453f1e6d6",
+    "url": "/hue-pals/static/css/main.7f3e3c89.chunk.css"
+  },
+  {
+    "revision": "f173fa74a96f06f2570e",
+    "url": "/hue-pals/static/js/2.78d9465c.chunk.js"
+  },
+  {
+    "revision": "c553331cd1b453f1e6d6",
+    "url": "/hue-pals/static/js/main.f2a284ba.chunk.js"
+  },
+  {
+    "revision": "34856a5a267dbb4549fb",
+    "url": "/hue-pals/static/js/runtime-main.5282c4f3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hue-pals/static/media/logo.25bf045c.svg"
+  }
+]);
